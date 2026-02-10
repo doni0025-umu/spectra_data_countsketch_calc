ab22d73d093eea631476d61f0febe45cca2d7891
douglas_beagle <douglas@beagle.henlab.org>
2026-02-10 11:36:19 +0100

    Cleaned up comments.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,10 @@ use indicatif::ProgressBar;
 use std::time::Duration;
 
 
-
-
-
 fn main() {
     let argmnts = args().collect::<Vec<String>>();
     let bar = ProgressBar::new_spinner();
     bar.enable_steady_tick(Duration::from_millis(100));
-    
-    // Below block for just running this script without commandArgs
-    
-    //let matrix = csv_to_linalg("/husky/douglas/sim/B_uniformis_no_to_all4plas_spectratest_260130__12_09/countsketch_mat.csv");
-    //let mut out_costheta_file  = BufWriter::new(std::fs::OpenOptions::new().write(true).truncate(true).create(true).open("/husky/douglas/sim/B_uniformis_no_to_all4plas_spectratest_260130__12_09/costheta.csv").expect("This path could NOT be used as output path. Maybe dir does not exist?"));
-    //bar.finish_with_message("Mean cosine thetas computed for all cells!");
-    ////////////////////////////////////////////////////////////////////////
 
 
     let matrix = csv_to_linalg(format!("/husky/douglas/sim/{}/countsketch_mat.csv", &argmnts[1]));
@@ -27,7 +17,10 @@ fn main() {
     let bar = ProgressBar::new_spinner();
     bar.enable_steady_tick(Duration::from_millis(100));
     for i in costheta_vec {
-        write!(out_costheta_file, "{}\n", i).expect("Problem writing the costheta info!");     
+        for costheta_val in i {
+            write!(out_costheta_file, "{},", costheta_val).expect("Problem writing the costheta info!");
+        }
+        out_costheta_file.write_all(b"\n").expect("Error when writing the newline xDDDDD!");
     }
     out_costheta_file.flush().expect("Problem flushing the costheta bufwriter!");
     bar.finish_with_message("Outfile csv written!");
@@ -54,9 +47,9 @@ fn csv_to_linalg(path: String) -> Vec<Vec<f64>> {
     rows
 }
 
-fn cos_theta_vector(matrix: Vec<Vec<f64>>) -> Vec<f64>{
+fn cos_theta_vector(matrix: Vec<Vec<f64>>) -> Vec<Vec<f64>>{
     matrix.iter().map(|row| {
-        (&matrix.iter().map(|comp_row| {
-            row.iter().zip(comp_row.iter()).map(|(x, y)| x * y).sum::<f64>() / (row.iter().map(|x| x*x).sum::<f64>().sqrt()*comp_row.iter().map(|x| x*x).sum::<f64>().sqrt())}).sum::<f64>()-1.0_f64) / (matrix.len()-1) as f64
-        }).collect::<Vec<f64>>()
+        matrix.iter().map(|comp_row| {
+            row.iter().zip(comp_row.iter()).map(|(x, y)| x * y).sum::<f64>() / (row.iter().map(|x| x*x).sum::<f64>().sqrt()*comp_row.iter().map(|x| x*x).sum::<f64>().sqrt())}).collect::<Vec<f64>>()
+        }).collect::<Vec<Vec<f64>>>()
 }
